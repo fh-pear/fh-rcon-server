18bf4ee036a017545b81ceab79945ea6e64decaf
fh-pear <pear.cod4@gmail.com>
2017-05-23 23:16:23 -0400

    added service class that included md5 hashing functions

---
+++ src/Function.java
@@ -0,0 +1,68 @@
+import java.io.UnsupportedEncodingException;
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+/**
+ * static service function class
+ */
+public class Function {
+
+    public static String getMD5(char[] array) {
+        String str = new String(array), hashtext = "";
+        byte[] bytesOfMessage = null;
+        byte[] thedigest = null;
+
+        try {
+
+            bytesOfMessage = str.getBytes("UTF-8");
+            str = "";
+
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            thedigest = md.digest(bytesOfMessage);
+
+            BigInteger bigInt = new BigInteger(1, thedigest);
+            hashtext = bigInt.toString(16);
+
+            while (hashtext.length() < 32) {
+                hashtext = "0" + hashtext;
+            }
+
+        } catch (UnsupportedEncodingException e) {
+            System.out.println("Your machine does not support UTF-8 encoding: " + e.getMessage());
+        } catch (NoSuchAlgorithmException e) {
+            System.out.println("Your machine does not support MD5 hashing: " + e.getMessage());
+        }
+
+        return hashtext;
+    }
+
+    public static String getMD5(String str) {
+        String hashtext = "";
+        byte[] bytesOfMessage = null;
+        byte[] thedigest = null;
+
+        try {
+
+            bytesOfMessage = str.getBytes("UTF-8");
+            str = "";
+
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            thedigest = md.digest(bytesOfMessage);
+
+            BigInteger bigInt = new BigInteger(1, thedigest);
+            hashtext = bigInt.toString(16);
+
+            while (hashtext.length() < 32) {
+                hashtext = "0" + hashtext;
+            }
+
+        } catch (UnsupportedEncodingException e) {
+            System.out.println("Your machine does not support UTF-8 encoding: " + e.getMessage());
+        } catch (NoSuchAlgorithmException e) {
+            System.out.println("Your machine does not support MD5 hashing: " + e.getMessage());
+        }
+
+        return hashtext;
+    }
+}
